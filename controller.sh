9f3337f3d3772c43a5db7baa6101a5b5df93f748
Sandeep Kumar <kkumar.sandeep89@gmail.com>
2015-12-29 15:55:15 +0530

    added controller
    controller

---
+++ controller.sh
@@ -0,0 +1,15 @@
+#export JAVA_HOME
+export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/jre"
+
+#export EC2_HOME
+export EC2_HOME="/usr/local/ec2/"
+
+#export EC2 key and secret
+export AWS_ACCESS_KEY=Your_key
+export AWS_SECRET_KEY=Your_secret
+
+#export ec2 endoint
+export EC2_URL=https://ec2.eu-west-1.amazonaws.com
+export EC2_HOME="/usr/local/ec2/"
+
+/usr/local/ec2/bin/$1
